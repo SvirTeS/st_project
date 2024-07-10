efc8a896e75e4d6681af7b5f09f21efedb9d8c9c
SvirT <svirtes@gmail.com>
2024-07-11 00:30:07 +0400

    rafactor contact helper

--- fixture/contact.py
+++ fixture/contact.py
@@ -15,11 +15,9 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
 
-    def create(self, contact):
+    def fill_contact_data(self, contact):
         wd = self.app.wd
-        self.open_page_to_add_contact()
         # fill contact data
-        # input name
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.firstname)
@@ -52,6 +50,12 @@ class ContactHelper:
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys(contact.byear)
+
+    def create(self, contact):
+        wd = self.app.wd
+        self.open_page_to_add_contact()
+        # fill contact data
+        self.fill_contact_data(contact)
         # save data
         wd.find_element_by_name("submit").click()
 
@@ -65,40 +69,10 @@ class ContactHelper:
 
     def mod_first_contact(self, contact):
         wd = self.app.wd
-        #select first group
+        self.open_home_page()
+        #select first contact
         wd.find_element_by_xpath("//*[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
-        #fill contact data
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        # input last name
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        # input nickname
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        # input title
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        # input company
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        # input address
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        # input home_phone
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_phone)
-        # input email
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        # input bday
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        # submit deletion
+        # fill contact data
+        self.fill_contact_data(contact)
+        # submit
         wd.find_element_by_name("update").click()

4df8babcc928a39c41a7035cb4e104fd760d36fe
SvirT <svirtes@gmail.com>
2024-08-09 03:31:45 +0400

    Добавлены новые параметры командной строки

--- fixture/application.py
+++ fixture/application.py
@@ -6,16 +6,24 @@ from fixture.group import GroupHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+    def __init__(self, browser, base_url):
+        if browser == 'chrome':
+            self.wd = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+        elif browser == 'firefox':
+            self.wd = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
+        elif browser == 'safari':
+            self.wd = webdriver.Safari(executable_path='/usr/bin/safaridriver')
+        else:
+            raise ValueError('Unrecognized browser %s' % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def open_home_page(self):
         wd = self.wd
         # open home page
-        wd.get("http://localhost/addressbook/index.php")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()

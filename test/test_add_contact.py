85e60d413d2410644d857b29e6020eb2d015d354
SvirT <svirtes@gmail.com>
2024-08-08 04:50:28 +0400

    Переделанные тесты с параметризацией

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + ' '*10
+    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = ([Contact(firstname='', lastname='', address='', home_phone='', email='')]
+            + [Contact(firstname=random_string('firstname', 6),
+                       lastname=random_string('lastname', 8),
+                       address=random_string('address', 15),
+                       home_phone=random_string('home_phone', 11),
+                       email=random_string('email', 20))])
+
+
+@pytest.mark.parametrize('contact', testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = (Contact(firstname='Name', lastname='LastName', nickname='nick', title='title', company='company', address='city17', home_phone='123456', email='email@example.com'))
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = (Contact(firstname='', lastname='', nickname='', title='', company='', address='', home_phone='', email=''))
-    app.contact.create(contact)
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-

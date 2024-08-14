4b727f84870388b4d226b3f92546a7527d756744
SvirT <svirtes@gmail.com>
2024-08-14 04:01:17 +0400

    add generators for group and contacts, read test data from json

--- generator/group.py
+++ generator/group.py
@@ -1,7 +1,7 @@
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 from model.group import Group
@@ -37,4 +37,5 @@ testdata = [Group(group_name='', group_header='', group_footer='')] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, 'w') as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options('json', indent=2)
+    out.write(jsonpickle.encode(testdata))

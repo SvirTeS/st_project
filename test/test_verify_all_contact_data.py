177311e909a7664733f08d792e8620e854fa3286
SvirT <svirtes@gmail.com>
2024-08-23 04:21:43 +0400

    Сравнение всех контатов из интерфейса и бд

--- test/test_verify_all_contact_data.py
+++ test/test_verify_all_contact_data.py
@@ -3,16 +3,17 @@ from random import randrange
 from model.contact import Contact
 
 
-def test_verify_all_contact_data(app):
-    all_contacts = app.contact.get_contact_list()
-    index = randrange(len(all_contacts))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+def test_verify_all_contact_data(app, db):
+    all_contacts_from_ui = app.contact.get_contact_list()
+    all_contacts_from_db = db.get_contact_list()
+    assert len(all_contacts_from_ui) == len(all_contacts_from_db)
+    for contact_ui in all_contacts_from_ui:
+        contact_db = next(filter(lambda c: c.id == contact_ui.id, all_contacts_from_db))
+        assert contact_ui.firstname == contact_db.firstname.strip()
+        assert contact_ui.lastname == contact_db.lastname.strip()
+        assert contact_ui.address == contact_db.address.strip()
+        assert contact_ui.all_phones_from_home_page == merge_phones_like_on_home_page(contact_db)
+        assert contact_ui.all_emails_from_home_page == merge_emails_like_on_home_page(contact_db)
 
 
 def clear(s):

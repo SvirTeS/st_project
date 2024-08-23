d86331ba5751cdff3e5ff59222fb28cf78b56306
SvirT <svirtes@gmail.com>
2024-08-23 04:40:51 +0400

    fix mod contacts

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -11,6 +11,5 @@ def test_mod_some_contact(app, db):
     contact_choice = random.choice(old_contacts)
     app.contact.mod_contact_by_id(contact_choice.id, contact)
     new_contacts = db.get_contact_list()
-    #old_contacts[index] = contact
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 

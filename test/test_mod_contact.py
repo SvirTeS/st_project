f105ea788b1a2c530c612bfc17dcc56fedb55ca0
SvirT <svirtes@gmail.com>
2024-08-26 00:18:47 +0400

    fix tests

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -2,7 +2,7 @@ from model.contact import Contact
 import random
 
 
-def test_mod_some_contact(app, db):
+def test_mod_some_contact(app, db, check_ui):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname='test'))
     old_contacts = db.get_contact_list()
@@ -11,5 +11,8 @@ def test_mod_some_contact(app, db):
     contact_choice = random.choice(old_contacts)
     app.contact.mod_contact_by_id(contact_choice.id, contact)
     new_contacts = db.get_contact_list()
-    assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    old_contacts = [contact if x == contact_choice else x for x in old_contacts]
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui is True:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -2,7 +2,7 @@ from model.group import Group
 import random
 
 
-def test_mod_some_group(app, db):
+def test_mod_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(group_name='test'))
     old_groups = db.get_group_list()
@@ -10,6 +10,9 @@ def test_mod_some_group(app, db):
     group = (Group(group_name="mod_group_name", group_header="mod_group_header", group_footer="mod_group_footer"))
     app.group.mod_group_by_id(group_choice.id, group)
     new_groups = db.get_group_list()
-    assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    old_groups = [group if x == group_choice else x for x in old_groups]
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui is True:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 
